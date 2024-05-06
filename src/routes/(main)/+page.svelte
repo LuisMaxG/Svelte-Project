c9681394bc1973149be1add06bb0da2cc3b38d48
LuisMaxG <luis_garciaibarra@hotmail.com>
2024-05-05 20:54:59 -0700

    Js Promises

---
+++ src/routes/(main)/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let data;
+    console.log(data);
+    const MiPromesa = () => {
+        return new Promise((resolve, reject) => {
+            console.log("estoy haciendo algo mas")
+            setTimeout(()=> {
+                resolve("se ha terminado todo");
+            }, 2000);
+        })
+    }
+
+    const MiPromesa2 = new Promise((resolve,rejact) => {
+        console.log("Estas haciendo algo");
+        setTimeout(()=>{
+            resolve(MiPromesa());
+        }, 2000)
+    })
+
+    MiPromesa2.then((resultado) => {
+        return resultado;
+    }).then((mensaje)=> {
+        console.log(mensaje);
+    }).catch((err) => {
+        console.err(err);
+    })
+</script>
+
+<h1>Shop</h1>
